eb8bc507b8638e9efe0ea8d86037f6dddafe1273
Keeri Tramm <35671162+keerilynn@users.noreply.github.com>
2022-09-12 16:10:11 -0500

    while loops, ++, and --

---
+++ while and ++.c
@@ -0,0 +1,28 @@
+//Assignment: Write program(s) using while loops and the ++ and -- operators to output the following sequences:
+// 1 2 3 4 5  1 2 3 4
+//1 2 3 4 5  4 3 2 1
+
+#include <stdio.h>
+int main(void)
+{
+	int i= 0;
+	while (i<5)
+		printf("%d ", ++i); //prints 1-5 with a space after each integer
+	
+	i= 1;
+	while (i<5)
+		printf(" %d", i++); //prints 1-4 with a space before each integer
+	
+	printf("\n"); //line break
+	
+	i= 0;
+	while (i<5)
+		printf("%d ", ++i); //prints 1-5 with a space after each integer
+		
+	while (i>1)
+		printf(" %d", --i);
+	
+	printf("\n");
+	
+	return 0;
+}
